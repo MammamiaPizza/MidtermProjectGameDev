bf67b19a8b720abd2524c2285eecc4103064e591
MammamiaPizza <thanaphumi.t@kkumail.com>
2025-08-12 17:28:03 +0700

    testfix

--- Player.gd
+++ Player.gd
@@ -2,19 +2,37 @@ extends CharacterBody2D
 
 var speed = 200
 var gravity = 3
+var stamina = 100
+
+func _ready() -> void:
+	$regenStamina.start()
 
 func _process(delta: float) -> void:
 
+	if Input.is_action_just_pressed("iframedodge"):	
+		dodge()
+
 	var axis = Input.get_axis("left", "right")
 	velocity = Vector2(axis * speed, velocity.y)
-	move_and_slide()
 	
 	if !is_on_floor():
 		velocity.y += gravity
 	
 	if Input.is_action_just_pressed("jump"):
-		velocity.y -= 150
+		velocity.y -= 200
 	
 	if Input.is_action_just_pressed("down"):	
 		position.y += 1
+		
+	move_and_slide()
 	
+func dodge():
+	if stamina > 20:
+			stamina = stamina - 20
+			speed = 800
+			await get_tree().create_timer(0.1).timeout
+			speed = 200
+
+func _on_regen_stamina_timeout() -> void:
+	if stamina<100:
+		stamina += 1
