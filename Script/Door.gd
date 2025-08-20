57945ae67d3aec2bd9abd47ce6c77a19f01c6615
MammamiaPizza <thanaphumi.t@kkumail.com>
2025-08-20 10:04:52 +0700

    fix in file doc

--- Script/Door.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@export var nextscene : PackedScene
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("player"):
-		get_tree().change_scene_to_packed(nextscene)
