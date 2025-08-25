8ebea29f5d679d894c518c257296daf300d5b461
MammamiaPizza <thanaphumi.t@kkumail.com>
2025-08-26 02:13:39 +0700

    make shadow trail and cd asset and item

--- Script/Player/Player.gd
+++ Script/Player/Player.gd
@@ -5,7 +5,7 @@ signal gethit
 var shadow = preload("res://Scene/shadow.tscn")
 
 var hp = 100
-var stamina = 1000000
+var stamina = 100
 
 var speed = 200
 var gravity = 3
@@ -14,27 +14,32 @@ var doublejump = true
 var alive = true
 var checkdash = false
 
+var i : int
 
 func _ready() -> void:
 	$regenStamina.start()
 	$Attack.hide()
-	#$Attack/CollisionShape2D.disabled = true
 
+#controlling part control
 func _process(delta: float) -> void:
+	#if alive it going to do
 	if alive:
+		#get axis left or right then set position
 		var axis = Input.get_axis("left", "right")
 		velocity = Vector2(axis * speed, velocity.y)
 		
+		#check is it floor if not gravity pull to ground and if yes recharge doublejump
 		if !is_on_floor():
 			velocity.y += gravity
 		elif is_on_floor():
 			doublejump = true
 
-		if Input.is_action_pressed("iframedodge"):	
+		if Input.is_action_just_pressed("iframedodge"):
 			dodge()
 			
-		if checkdash:
-			add_child(shadow)
+		#make Shadow after dash is on (Make A lot)
+		#if checkdash:
+			#createTrail()
 		
 		if Input.is_action_just_pressed("jump"):
 			jumping()
@@ -44,13 +49,9 @@ func _process(delta: float) -> void:
 		
 		if (axis > 0):
 			$Sprite2D.flip_h = false
-			#$Attack/AnimatedSprite2D.flip_h = false
-			#$Attack/CollisionShape2D.scale.x *= 1
 			$Attack.scale.x = 1
 		elif (axis < 0):
 			$Sprite2D.flip_h = true
-			#$Attack/AnimatedSprite2D.flip_h = true
-			#$Attack/CollisionShape2D.scale.x *= -1
 			$Attack.scale.x = -1
 		
 		if Input.is_action_just_pressed("AttackMelee"):
@@ -59,30 +60,37 @@ func _process(delta: float) -> void:
 			
 		move_and_slide()
 	
+	#check dead
 	if hp <= 0:
 		alive = false
 		#queue_free()
 		hide()
 	
 func dodge():
+	#before start should make all component ready to open
 	$Attack/AnimatedSprite2D.set_frame(0)
 	$Attack/CollisionShape2D.disabled = true
 	$Attack.hide()
+	#checkdash link to createTrail in Process
 	if stamina > 20:
 			$Hitbox/CollisionShape2D.disabled = true
-			checkdash = true
+			#checkdash = true
 			stamina = stamina - 20
 			speed = 400
+			$ShadowTrail.start()
 			await get_tree().create_timer(0.3).timeout
-			#await get_tree().create_timer(5).timeout
-			checkdash = false
+			#checkdash = false
 			speed = 200
 			$Hitbox/CollisionShape2D.disabled = false
+			$ShadowTrail.stop()
 
+#create Shadow follow after dash
 func createTrail() -> void:
 	var trail = shadow.instantiate()
-	trail.location = global_position
+	trail.position = position
 	trail.scale = scale
+	get_tree().current_scene.add_child(trail)
+	
 
 func _on_regen_stamina_timeout() -> void:
 	if stamina<100:
@@ -107,3 +115,7 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("attackByBoss"):
 		var getdamagefromattack = area.getDamage()
 		hp -= getdamagefromattack
+
+
+func _on_shadow_trail_timeout() -> void:
+	createTrail()
--- Script/Player/shadow.gd
+++ Script/Player/shadow.gd
@@ -3,10 +3,12 @@ extends Sprite2D
 func _ready() -> void:
 	fadeout()
 
+
+#make it fade
 func fadeout() -> void:
 	var tween = get_tree().create_tween()
 	
-	tween.tween_property(self, "self_modulate", Color(1,1,1,0), 0.75)
+	tween.tween_property(self, "self_modulate", Color(1,1,1,0), 0.5)
 	await tween.finished
 	
 	queue_free()
