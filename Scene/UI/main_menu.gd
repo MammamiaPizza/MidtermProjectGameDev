c648b195a342d8ddba574e028ec6d919fa0e442a
punnavit-prongsavaroj <punnavit.p@kkumail.com>
2025-09-01 21:40:26 +0700

    door

--- Scene/UI/main_menu.gd
+++ Scene/UI/main_menu.gd
@@ -6,6 +6,6 @@ func _on_button_pressed() -> void:
 	var loading_scene = preload("res://Scene/load/LoadingScreen.tscn").instantiate()
 	$CanvasLayer.visible = false
 
-	loading_scene.target_scene = "res://Scene/UI/Setting.tscn"
+	loading_scene.target_scene = "res://Scene/UI/select-door.tscn"
 	get_tree().current_scene.add_child(loading_scene)
 	pass
