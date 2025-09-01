01b037a499017bc0ec580e9a321c450c7e90a9e3
punnavit-prongsavaroj <punnavit.p@kkumail.com>
2025-09-01 16:47:31 +0700

    finnish-loading-sceen

--- Scene/load/LoadingScreen.gd
+++ Scene/load/LoadingScreen.gd
@@ -1,22 +1,37 @@
 extends Node2D
 # Loading.gd# LoadingScreen.gd
 
-
 var loader
 var load_progress = 0.0
-@export var target_scene = "none"
-
+@export var target_scene = null
+var checking = true
+var loaded_scene = null
 
 func _ready():
 	# เริ่มโหลดแบบ background
 	$AnimatedSprite2D.play("start")
-	loader = ResourceLoader.load(target_scene)
+	await $AnimatedSprite2D.animation_finished
+	if target_scene != "":
+		# ใช้ threaded request แทน interactive
+		var err = ResourceLoader.load_threaded_request(target_scene)
+		if err != OK:
+			push_error("โหลด scene ไม่ได้: %s" % target_scene)
+	print(target_scene)
 
 func _process(delta):
-	if loader:
-		var err = loader.poll()
-		if err == ERR_FILE_EOF:
-			# โหลดเสร็จ → เอา scene มาใช้
-			$AnimatedSprite2D.play("stop")
-			var scene = loader.get_resource()
-			get_tree().change_scene_to_packed(scene)
+	if checking and target_scene != "":
+		var status = ResourceLoader.load_threaded_get_status(target_scene)
+
+		if status == ResourceLoader.THREAD_LOAD_LOADED:
+			checking = false   # ✅ ปิด loop ไม่ให้เข้าอีก
+			_start_transition()
+
+func _start_transition() -> void:
+	# โหลดเสร็จแล้ว ดึง scene มาเก็บ
+	loaded_scene = ResourceLoader.load_threaded_get(target_scene)
+	# เล่น stop animation
+	$AnimatedSprite2D.play("stop")
+	# รอจนกว่าจะจบ
+	await $AnimatedSprite2D.animation_finished
+	# พอจบแล้วเปลี่ยน scene
+	get_tree().change_scene_to_packed(loaded_scene)
