144bd674ecb8669b5ab5d50265736bd730b84164
MammamiaPizza <thanaphumi.t@kkumail.com>
2025-09-01 17:49:17 +0700

    ?

--- Scene/Level/dead_level.gd
+++ Scene/Level/dead_level.gd
@@ -13,36 +13,34 @@ var tower4 = towerScene.instantiate()
 
 func _ready() -> void:
 	createTower()
-	tower.hide()
+	
 
 func _on_dead_change_phase() -> void:
 	get_tree().paused = true
 	changePhase = true
 	createTower()
+	await get_tree().create_timer(2).timeout
+	get_tree().paused = false
 	
 
 func createTower():
 	add_child(tower)
 	tower.scale = $Dead.scale
 	tower.position = Vector2(-500,-500)
+	tower.setTarget($TowerLocate.position)
 	add_child(tower2)
 	tower2.scale = $Dead.scale
 	tower2.position = Vector2(2000,-500)
+	tower2.setTarget($TowerLocate2.position)
 	add_child(tower3)
 	tower3.scale = $Dead.scale
 	tower3.position = Vector2(-500,500)
+	tower3.setTarget($TowerLocate3.position)
 	add_child(tower4)
 	tower4.scale = $Dead.scale
 	tower4.position = Vector2(2000,500)
+	tower4.setTarget($TowerLocate4.position)
 	
-	
-func _process(delta: float) -> void:
-	if changePhase:
-		var direction1 = $TowerLocate.position - tower.position
-		tower.position += direction1 * delta
-		var direction2 = $TowerLocate2.position - tower2.position
-		tower2.position += direction2 * delta
-		var direction3 = $TowerLocate3.position - tower3.position
-		tower3.position += direction3 * delta
-		var direction4 = $TowerLocate4.position - tower4.position
-		tower4.position += direction4 * delta
+func TowerOpen():
+	print("Hello")
+	towerPowerOn.emit()
--- Script/Death/tower.gd
+++ Script/Death/tower.gd
@@ -1,19 +1,25 @@
 extends StaticBody2D
 
-signal Turnon
+signal TowerIsOpen
 
 var hp : int = 7
-var turnon= false
+
+var target : Vector2
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("attackByPlayer"):
-		print("attackTower")
+		#print("attackTower")
 		hp -= 1
 		if hp <= 0:
 			$Area2D/CollisionShape2D.disabled = true
 			$Area2D.queue_free()
-			turnon = true
-			turnonTower()
 
-func turnonTower():
-	turnon.emit()
+
+func _process(delta: float) -> void:
+	if target != null:
+		var direction1 = target - position
+		position += direction1 * delta
+	
+func setTarget(targetposition : Vector2) -> void:
+	target = targetposition
+	
