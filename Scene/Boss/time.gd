e07a84f12d77d04f65a961fbc7f28e3f3b523d23
punnavit-prongsavaroj <punnavit.p@kkumail.com>
2025-09-02 16:52:18 +0700

    time

--- Scene/Boss/time.gd
+++ Scene/Boss/time.gd
@@ -2,29 +2,35 @@ extends CharacterBody2D
 
 var speed = 200
 @onready var target : CharacterBody2D = get_tree().get_nodes_in_group("player")[0]
-var hp = 10000
+var hp = 100
 var attacking = false
 var random
 var exhp
 var randommove = 1
 var timemark = -1
+var phasetwo = false
 func _ready() -> void:
-	pass
+	$AnimatedSprite2D.play("idle")
+func _process(delta: float) -> void:
+	$Label.set_text(str(int(($ResetWorld.get_time_left()))))
 
-func _on_timermove_timeout() -> void:
-	randommove.randi(1,3)
 	if randommove == 1:
 		var dir = Vector2((620 -position.x),(88-position.y)).normalized()
-		velocity = dir * speed
-		move_and_slide()
+		velocity = dir * speed * delta
+		
 	elif  randommove == 2:
 		var dir = Vector2((66 -position.x),(474-position.y)).normalized()
 		velocity = dir * speed
-		move_and_slide()
+		
 	else:
-		var dir = Vector2((1168 -position.x),(527-position.y)).normalized()
+		var dir = Vector2((1200 -position.x),(500-position.y)).normalized()
 		velocity = dir * speed
-		move_and_slide()
+	
+	move_and_slide()
+	
+func _on_timermove_timeout() -> void:
+	randommove = randi_range(1, 3)
+	
 		
 func _on_random_attack_timeout() -> void:
 	timemark *= -1
@@ -43,9 +49,26 @@ func _on_hit_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("attackByPlayer"):
 		hp -= area.getDamage()
 		print(hp)
-
+	if hp < 0 :
+		if !phasetwo:
+			phasetwo = true
+			$ResetWorld.start()
+			$Label.visible = true
+			hp = 10000
+			$"../AudioStreamPlayer2D".stop()
+			$"../AudioStreamPlayer2D2".play()
+		else:
+			pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("attackByPlayer"):
 		exhp -= area.getDamage()
 		print(exhp)
+
+
+func _on_reset_world_timeout() -> void:
+	phasetwo = false
+	hp = 10000
+	$Label.visible = false
+	$"../AudioStreamPlayer2D".play()
+	$"../AudioStreamPlayer2D2".stop()
