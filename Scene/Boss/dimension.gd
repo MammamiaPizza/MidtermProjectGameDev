b0151a18f8f575f16b5f0bf90e0ba0317f3f59e6
punnavit-prongsavaroj <punnavit.p@kkumail.com>
2025-09-02 10:37:29 +0700

    adw

--- Scene/Boss/dimension.gd
+++ Scene/Boss/dimension.gd
@@ -20,81 +20,81 @@ func _ready() -> void:
 
 
 func _on_attack_time_timeout() -> void:
-	
-	randommove = Moveset.pick_random()
-	if gravityset != 0 :
-		gravityset = randi_range(1,4)
-		target.gravity_set = gravityset
-		$".".rotateevery(gravityset)
-	#print(target.position.x)
-	#print(position.x)
-	if !sealcheck:
-		if randommove == "Attack1":
-			$body.play("warp")
-			await $body.animation_finished
-			position = target.global_position
-			$body.play_backwards("warp")
-			await $body.animation_finished
-			$body.play(randommove)
-			await get_tree().create_timer(0.6).timeout
-			$Hit/Attack1.disabled = false
-			await get_tree().create_timer(0.2).timeout
-			$Hit/Attack1.disabled = true
-			await $body.animation_finished
-		
-		elif randommove == "Attack2":
-			if (position.x > target.position.x):
+	if hp > 0 :
+		randommove = Moveset.pick_random()
+		if gravityset != 0 :
+			gravityset = randi_range(1,4)
+			target.gravity_set = gravityset
+			$".".rotateevery(gravityset)
+		#print(target.position.x)
+		#print(position.x)
+		if !sealcheck:
+			if randommove == "Attack1":
 				$body.play("warp")
 				await $body.animation_finished
-				position.x = target.global_position.x+100
-				position.y = target.global_position.y
+				position = target.global_position
 				$body.play_backwards("warp")
 				await $body.animation_finished
-				$body.scale *= -1
-				$body.play("Attack2")
+				$body.play(randommove)
+				await get_tree().create_timer(0.6).timeout
+				$Hit/Attack1.disabled = false
+				await get_tree().create_timer(0.2).timeout
+				$Hit/Attack1.disabled = true
 				await $body.animation_finished
-				$body.scale *= -1
-				$Hit/AnimatedSprite2D.set_frame_and_progress(0, 0)
-				$Hit/AnimatedSprite2D.play("Attack2")
-				$Hit/AnimatedSprite2D.show()
-				
-				await $Hit/AnimatedSprite2D2.animation_finished
-				$Hit/Attack2.disabled = false
-				await get_tree().create_timer(0.05).timeout
-				$Hit/Attack2.disabled = true
-				
-			elif (position.x < target.position.x):
-				$body.play("warp")
-				await $body.animation_finished
-				position.x = target.global_position.x-100
-				position.y = target.global_position.y
-				$body.play_backwards("warp")
-				await $body.animation_finished
-				$body.play("Attack2")
-				await $body.animation_finished
-				$Hit/AnimatedSprite2D3.set_frame_and_progress(0, 0)
-				$Hit/AnimatedSprite2D3.play("Attack3")
-				$Hit/AnimatedSprite2D3.show()
-				
-				await $Hit/AnimatedSprite2D2.animation_finished
-				$Hit/Attack3.disabled = false
-				await get_tree().create_timer(0.05).timeout
-				$Hit/Attack3.disabled = true
 			
-		elif randommove == "Attack4":
-			position = target.global_position
-			$body.play(randommove)
-			await get_tree().create_timer(0.6).timeout
-			$Hit/Attack4.disabled = false
-			await get_tree().create_timer(0.2).timeout
-			$Hit/Attack4.disabled = true
-			await $body.animation_finished
-		
-		$body.play("Idle")
-		$Hit/AnimatedSprite2D2.hide()
-		$CooldownMove.start()
-	else:
-		return
+			elif randommove == "Attack2":
+				if (position.x > target.position.x):
+					$body.play("warp")
+					await $body.animation_finished
+					position.x = target.global_position.x+100
+					position.y = target.global_position.y
+					$body.play_backwards("warp")
+					await $body.animation_finished
+					$body.scale *= -1
+					$body.play("Attack2")
+					await $body.animation_finished
+					$body.scale *= -1
+					$Hit/AnimatedSprite2D.set_frame_and_progress(0, 0)
+					$Hit/AnimatedSprite2D.play("Attack2")
+					$Hit/AnimatedSprite2D.show()
+					
+					await $Hit/AnimatedSprite2D2.animation_finished
+					$Hit/Attack2.disabled = false
+					await get_tree().create_timer(0.05).timeout
+					$Hit/Attack2.disabled = true
+					
+				elif (position.x < target.position.x):
+					$body.play("warp")
+					await $body.animation_finished
+					position.x = target.global_position.x-100
+					position.y = target.global_position.y
+					$body.play_backwards("warp")
+					await $body.animation_finished
+					$body.play("Attack2")
+					await $body.animation_finished
+					$Hit/AnimatedSprite2D3.set_frame_and_progress(0, 0)
+					$Hit/AnimatedSprite2D3.play("Attack3")
+					$Hit/AnimatedSprite2D3.show()
+					
+					await $Hit/AnimatedSprite2D2.animation_finished
+					$Hit/Attack3.disabled = false
+					await get_tree().create_timer(0.05).timeout
+					$Hit/Attack3.disabled = true
+				
+			elif randommove == "Attack4":
+				position = target.global_position
+				$body.play(randommove)
+				await get_tree().create_timer(0.6).timeout
+				$Hit/Attack4.disabled = false
+				await get_tree().create_timer(0.2).timeout
+				$Hit/Attack4.disabled = true
+				await $body.animation_finished
+			
+			$body.play("Idle")
+			$Hit/AnimatedSprite2D2.hide()
+			$CooldownMove.start()
+		else:
+			return
 	
 
 func _on_hit_area_area_entered(area: Area2D) -> void:
