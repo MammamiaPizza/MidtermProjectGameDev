53f0010011dcc15a735436e76b3a692d956fda8a
MammamiaPizza <thanaphumi.t@kkumail.com>
2025-09-03 07:12:50 +0700

    fix

--- Script/main_menu (2).gd
+++ Script/main_menu (2).gd
@@ -9,3 +9,7 @@ func _on_button_pressed() -> void:
 	loading_scene.target_scene = "res://Scene/UI/select-door.tscn"
 	get_tree().current_scene.add_child(loading_scene)
 	pass
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
